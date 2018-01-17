6c17ab287ae4410c96d477a9f9159db513cad309
Shawpnochari13 <shawpnochari13@gmail.com>
2018-01-17 12:35:36 +0600

    onitemclick

--- app/src/main/java/com/techjany/customlist/MainActivity.java
+++ app/src/main/java/com/techjany/customlist/MainActivity.java
@@ -1,18 +1,24 @@
 package com.techjany.customlist;
-
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
 
     GridView listView;
+    MediaPlayer mp;
 
     String[] countryNames = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X","","", "Y", "Z","",""};
+            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+    int[] alphabet = {R.raw.a, R.raw.b, R.raw.c, R.raw.d, R.raw.e, R.raw.f, R.raw.g, R.raw.h, R.raw.i, R.raw.j, R.raw.k, R.raw.l, R.raw.m, R.raw.n,
+            R.raw.o, R.raw.p, R.raw.q, R.raw.r, R.raw.s, R.raw.t, R.raw.u, R.raw.v, R.raw.w, R.raw.x, R.raw.y, R.raw.z};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,24 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         MyAdapter myAdapter = new MyAdapter(MainActivity.this, countryNames);
         listView.setAdapter(myAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                try {
+                    if (mp.isPlaying())
+                        mp.stop();
+                        mp.release();
+                }catch (Exception e){
+
+                }
+               mp = MediaPlayer.create(MainActivity.this, alphabet[i]);
+               mp.start();
+
+
+            }
+        });
 
     }
 }
