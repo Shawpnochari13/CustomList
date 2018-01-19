b16356aa343d2eadfb29853af2514742f08a480c
Md. Azizul Haque Jany <shawpnochari13@gmail.com>
2018-01-20 00:49:16 +0600

    init

---
+++ app/src/main/java/com/techjany/customlist/Alphabet.java
@@ -0,0 +1,54 @@
+package com.techjany.customlist;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+public class Alphabet extends AppCompatActivity {
+
+    GridView listView;
+    MediaPlayer mp;
+
+    String[] countryNames = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+    int[] alphabet = {R.raw.a, R.raw.b, R.raw.c, R.raw.d, R.raw.e, R.raw.f,
+            R.raw.g, R.raw.h, R.raw.i, R.raw.j, R.raw.k, R.raw.l, R.raw.m, R.raw.n,
+            R.raw.o, R.raw.p, R.raw.q, R.raw.r, R.raw.s, R.raw.t, R.raw.u, R.raw.v, R.raw.w, R.raw.x, R.raw.y, R.raw.z};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_alphabet);
+
+        listView = findViewById(R.id.listView);
+        MyAdapter myAdapter = new MyAdapter(Alphabet.this, countryNames);
+        listView.setAdapter(myAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                try {
+                    if (mp.isPlaying())
+                        mp.stop();
+                    mp.release();
+                }catch (Exception e){
+
+                }
+                mp = MediaPlayer.create(Alphabet.this, alphabet[i]);
+                mp.start();
+
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/techjany/customlist/MainActivity.java
+++ app/src/main/java/com/techjany/customlist/MainActivity.java
@@ -1,4 +1,5 @@
 package com.techjany.customlist;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -6,19 +7,13 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
+Button btnAlphabet, btnWord;
 
-    GridView listView;
-    MediaPlayer mp;
-
-    String[] countryNames = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-
-    int[] alphabet = {R.raw.a, R.raw.b, R.raw.c, R.raw.d, R.raw.e, R.raw.f, R.raw.g, R.raw.h, R.raw.i, R.raw.j, R.raw.k, R.raw.l, R.raw.m, R.raw.n,
-            R.raw.o, R.raw.p, R.raw.q, R.raw.r, R.raw.s, R.raw.t, R.raw.u, R.raw.v, R.raw.w, R.raw.x, R.raw.y, R.raw.z};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,27 +23,19 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.listView);
-        MyAdapter myAdapter = new MyAdapter(MainActivity.this, countryNames);
-        listView.setAdapter(myAdapter);
+        btnAlphabet = findViewById(R.id.btnAlphabet);
+        btnWord = findViewById(R.id.btnWord);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        btnAlphabet.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                try {
-                    if (mp.isPlaying())
-                        mp.stop();
-                        mp.release();
-                }catch (Exception e){
-
-                }
-               mp = MediaPlayer.create(MainActivity.this, alphabet[i]);
-               mp.start();
-
-
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Alphabet.class);
+                startActivity(intent);
             }
         });
+
+
+
 
     }
 }
